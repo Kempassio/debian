56b266d9fc513464224fd99ddd5e983a625130f8
Kempassio <94110817+Kempassio@users.noreply.github.com>
2022-01-24 10:22:57 +0000

    Update red.sh

--- scripts/red.sh
+++ scripts/red.sh
@@ -2,13 +2,8 @@
 clear
 apt -y install net-tools
 clear
-echo "¿Que interfaz de red deseas modificar?"
-read red
-clear
-echo "¿dhcp o static?"
-read tipo
-if [ $tipo = 'dhcp' ]
-then
+#============Functions===============
+ifaced () {
 echo "This file describes the network interfaces available on your system" > /etc/network/interfaces
 echo "and how to activate them. For more information, see interfaces(5)." >> /etc/network/interfaces
 echo "" >> /etc/network/interfaces
@@ -20,13 +15,8 @@ echo "iface lo inet loopback" >> /etc/network/interfaces
 echo "" >> /etc/network/interfaces
 echo "auto $red" >> /etc/network/interfaces
 echo "iface $red inet $tipo" >> /etc/network/interfaces
-else
-echo "Direccion IP nueva: "
-read ip
-clear
-echo "Mascara de Red: "
-read mask
-clear
+}
+ifaces () {
 echo "This file describes the network interfaces available on your system" > /etc/network/interfaces
 echo "and how to activate them. For more information, see interfaces(5)." >> /etc/network/interfaces
 echo "" >> /etc/network/interfaces
@@ -41,5 +31,66 @@ echo "iface $red inet $tipo" >> /etc/network/interfaces
 echo "  address $ip" >> /etc/network/interfaces
 echo "  netmask $mask" >> /etc/network/interfaces
 echo "  dns-nameservers $ip" >> /etc/network/interfaces
+}
+#====================================
+echo "¿Cuantas tarjetas de red tienes?"
+read num
+clear
+if [ $num = 1]
+then
+echo "¿Que interfaz de red deseas modificar?"
+read red
+clear
+echo "¿dhcp o static?"
+read tipo
+    if [ $tipo = 'dhcp' ]
+    then
+    ifaced
+    else
+    echo "Direccion IP nueva: "
+    read ip
+    clear
+    echo "Mascara de Red: "
+    read mask
+    clear
+    ifaces
+    fi 
+else
+   echo "¿Que interfaz de red deseas modificar?"
+    read red
+    clear
+    echo "¿dhcp o static?"
+    read tipo
+    if [ $tipo = 'dhcp' ]
+    then
+    ifaced
+    else
+    echo "Direccion IP nueva: "
+    read ip
+    clear
+    echo "Mascara de Red: "
+    read mask
+    clear
+    ifaces
+    fi
+    echo "¿Que interfaz de red deseas modificar?"
+    read red
+    clear
+    echo "¿dhcp o static?"
+    read tipo
+    if [ $tipo = 'dhcp' ]
+    then
+    ifaced
+    else
+    echo "Direccion IP nueva: "
+    read ip
+    clear
+    echo "Mascara de Red: "
+    read mask
+    clear
+    ifaces
+    fi
+
 fi
+clear
 ifconfig | grep -A 1 $red
