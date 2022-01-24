a75d78e947fb62f87938e1b6db7a403ddafd32e2
Kempassio <94110817+Kempassio@users.noreply.github.com>
2022-01-24 08:19:11 +0000

    Add files via upload

---
+++ kerberos.sh
@@ -0,0 +1,15 @@
+sudo apt -y install samba smbclient winbind krb5-config
+#hacer una copia de los archivos
+mv /etc/samba/samba.conf /etc/samba/samba.conf.copy
+#Nuevo fichero de configuracion de samba
+cd /etc/samba
+samba-tool domain provision
+#mover el archivo que nos creo anteriormente
+mv /var/lib/samba/private/krb5.conf /etc
+#ejecutar estos comandos
+systemctl stop smbd winbind systemd-resolved
+systemctl disable smbd nmbd winbind system-resolved
+systemctl unmask samba-ad-dc
+systemctl enable samba-ad-dc
+systemctl restart samba-ad-dc
+
